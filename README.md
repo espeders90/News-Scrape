caac6ef8937ca85d22f738bcab0d7b0e97f80b3f
Eddie Pedersen <eddiepedersen90@gmail.com>
2018-01-08 19:26:19 +0000

    file/folder structure

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # News-Scrape
+
+MONGODB_URI
+mongodb://heroku_ml3glxph:ref82a984eoueblh2lgepdbgbc@ds247587.mlab.com:47587/heroku_ml3glxph
---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+var logger = require("morgan");
+
+var app = express();
+
+var db = require("./models");
+
+var PORT = 8080;
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: false }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
