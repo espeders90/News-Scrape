682ad088456b9d53d071177016ae34a1868eb9b3
Eddie Pedersen <eddiepedersen90@gmail.com>
2018-01-10 23:59:41 +0000

    sofar

--- public/app.js
+++ public/app.js
@@ -52,31 +52,31 @@ $(document).on("click", "p", function() {
         });
 });
 
-// When you click the savecomment button
-// $(document).on("click", "#savecomment", function() {
-//     // Grab the id associated with the article from the submit button
-//     var thisId = $(this).attr("data-id");
+// savecomment button
+$(document).on("click", "#savecomment", function() {
+    // Grab the id associated with the article from the submit button
+    var thisId = $(this).attr("data-id");
 
-//     // Run a POST request to change the comment, using what's entered in the inputs
-//     $.ajax({
-//             method: "POST",
-//             url: "/articles/" + thisId,
-//             data: {
-//                 // Value taken from title input
-//                 title: $("#titleinput").val(),
-//                 // Value taken from the comments textarea
-//                 body: $("#bodyinput").val()
-//             }
-//         })
-//         // With that done
-//         .done(function(data) {
-//             // Log the response
-//             console.log(data);
-//             // Empty the comments section
-//             $("#notes").empty();
-//         });
+    // Run a POST request to change the comment, using what's entered in the inputs
+    $.ajax({
+            method: "POST",
+            url: "/articles/" + thisId,
+            data: {
+                // Value taken from title input
+                title: $("#titleinput").val(),
+                // Value taken from the comments textarea
+                body: $("#bodyinput").val()
+            }
+        })
+        // With that done
+        .done(function(data) {
+            // Log the response
+            console.log(data);
+            // Empty the comments section
+            $("#notes").empty();
+        });
 
-//     // Also, remove the values entered in the input and textarea for comment entry
-//     $("#titleinput").val("");
-//     $("#bodyinput").val("");
-// });
+    // Also, remove the values entered in the input and textarea for comment entry
+    $("#titleinput").val("");
+    $("#bodyinput").val("");
+});
